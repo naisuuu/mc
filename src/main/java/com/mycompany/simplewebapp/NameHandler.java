32c2e1b7a92369ae113d8d5b233e9030228e759b
naisuuu <cjmosawan@yahoo.ie>
2020-10-29 00:21:39 +0000

    deploying project to github

---
+++ src/main/java/com/mycompany/simplewebapp/NameHandler.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.simplewebapp;
+
+/**
+ *
+ * @author cjmos
+ */
+public class NameHandler {
+    String name;
+    
+    public NameHandler(){
+        name = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
